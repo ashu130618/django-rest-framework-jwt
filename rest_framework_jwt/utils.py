734be3470b18cdbe0fe37efde32d53166dbfecac
ragupathymani <ragupathy@blowhorn.com>
2025-03-25 13:51:27 +0530

    decode logic version fix

--- rest_framework_jwt/utils.py
+++ rest_framework_jwt/utils.py
@@ -103,11 +103,21 @@ def jwt_decode_handler(token):
     # get user from token, BEFORE verification, to get user secret key
     unverified_payload = jwt.decode(token, None, False)
     secret_key = jwt_get_secret_key(unverified_payload)
+    # return jwt.decode(
+    #     token,
+    #     api_settings.JWT_PUBLIC_KEY or secret_key,
+    #     api_settings.JWT_VERIFY,
+    #     options=options,
+    #     leeway=api_settings.JWT_LEEWAY,
+    #     audience=api_settings.JWT_AUDIENCE,
+    #     issuer=api_settings.JWT_ISSUER,
+    #     algorithms=[api_settings.JWT_ALGORITHM]
+    # )
+
     return jwt.decode(
         token,
         api_settings.JWT_PUBLIC_KEY or secret_key,
-        api_settings.JWT_VERIFY,
-        options=options,
+        options={"verify_signature": api_settings.JWT_VERIFY, **options},
         leeway=api_settings.JWT_LEEWAY,
         audience=api_settings.JWT_AUDIENCE,
         issuer=api_settings.JWT_ISSUER,
